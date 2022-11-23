960571e5afbbd8172cee60539950e5ac0d7f6f7e
elvmariga <elvmariga@gmail.com>
2022-11-23 12:16:35 +0300

    partner map()

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -3,31 +3,30 @@ import PropTypes from "prop-types";
 import "./partners-style/partners-style.css";
 import userIcon from "./assets/user-plus.svg";
 import Partner from "../Partner/Partner";
-import { partnerIcons } from "./assets/partnerIcons.js";
+import {partnerIcon} from "./assets/partnerIcons.js";
 
 const Partners = () => {
-  return;
+  return (
+    <div className="partners">
+      <div className="partners-content">
+        <div className="partners-top">
+          <h2>Partners</h2>
+        </div>
+        <div className="partners-bottom">
+          <div className="join">
+            <img src={userIcon} alt="use plus icon" />
+            <p>Join us</p>
+          </div>
 
-  <div className="partners">
-    <div className="partners-top">
-      <h2>Partners</h2>
-    </div>
-    <div className="partners-bottom">
-      <div className="join">
-        <img src={userIcon} alt="use plus icon" />
-        <p>Join us</p>
-      </div>
-
-      <div>
-        {partnerIcons.map((image) => {return (
-          <img src={image} alt="partner" />
-        )
-        )
-      }
-    }
+          <div className="the-partners">
+            {partnerIcon.map((image) => {
+              return <img src={image} alt="partner" />;
+            })}
+          </div>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default Partners;
--- src/components/Partners/assets/partnerIcons.js
+++ src/components/Partners/assets/partnerIcons.js
@@ -1,4 +1,8 @@
-import day from './daycare.png'
-import kcpe from './KCPE-certification.png'
+import p1 from './p1.svg'
+import p2 from "./p2.svg";
+import p3 from "./p3.svg";
+import p4 from "./p4.svg";
+import p5 from "./p5.svg";
+import p6 from "./p6.svg";
 
-export const partner = [day, kcpe ]
+export const partnerIcon = [p1, p2, p3, p4, p5, p6 ];
