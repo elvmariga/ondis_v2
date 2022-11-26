0ff148138d07a1585e5a39df9d94931c034d2ed2
elvmariga <elvmariga@gmail.com>
2022-11-26 10:34:31 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ export const App = () => {
     <section>
      
     <Navbar/>
-    <Routes>
+    
+    {/* <Routes>
       <Route
       path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element= {<Home/>} />
       <Route path="/about" element= {<About/>} />
       <Route path="/contact" element ={<Contact/>} />
-    </Routes>
+    </Routes> */}
     <Footer/>
       
     </section>
