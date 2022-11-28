041f64fe32207c17c89b0fe49dc9cfce421822f0
boaz serem <boaserem022@gmail.com>
2022-11-28 11:02:24 +0300

    progress

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,18 @@ import '../Navbar/navbar-style/navbar-style.css';
 import logo from "./assests/ondis-logo.svg";
 import Button from '../Button/Button';
 import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
 
-export const Navbar = () => (
+export const Navbar = () => {
+  const [showLinks, setShowLinks] = React.useState(false);
+  // const [showModal, setShowModal] = React.useState(false);
+
+  const handleClick = () => {
+    setShowLinks(!showLinks);
+  };
+  
+  return(
   <div className="navbar">
     <div className="navbar-content">
       <div className="logo">
@@ -48,6 +58,89 @@ export const Navbar = () => (
         </ul>
       </div>
     </div>
-  </div>
-);
+
+    <div style={{ marginRight: "0.5rem" }}>
+            <div className="humberger" onClick={() => handleClick()}>
+              {showLinks ? <ImCross /> : <GiHamburgerMenu />}
+            </div>
+
+            <div className="mobile-container">
+              {showLinks && (
+                <div className="mobilelinks">
+                  <ul
+                    style={{
+                      listStyle: "none",
+                      textDecoration: "none",
+                      color: "black",
+                    }}
+                  >
+                    <li
+                      className="link"
+                      onClick={() => handleClick()}
+                      style={{
+                        textDecoration: "none",
+                        fontSize: "1.3rem",
+                        fontFamily: "Poppins",
+                      }}
+                    >
+                      <NavLink
+                        smooth="true"
+                        spy="true"
+                        style={{
+                          textDecoration: "none",
+                          fontSize: "1.3rem",
+                          fontFamily: "Poppins",
+                          color: "#141138",
+                        }}
+                        activeclassname="active"
+                        to="/home"
+                      >
+                        Home
+                      </NavLink>
+                    </li>
+                    <hr />
+                    <li className="link" onClick={() => handleClick()}>
+                      <NavLink
+                        smooth="true"
+                        spy="true"
+                        style={{
+                          textDecoration: "none",
+                          fontSize: "1.3rem",
+                          fontFamily: "Poppins",
+                          color: "#141138",
+                        }}
+                        activeClassName="active"
+                        to="/about"
+                      >
+                        About 
+                      </NavLink>
+                    </li>
+                    <hr />
+                    <li className="link" onClick={() => handleClick()}>
+                      <NavLink
+                        smooth="true"
+                        spy="true"
+                        style={{
+                          textDecoration: "none",
+                          fontSize: "1.3rem",
+                          fontFamily: "Poppins",
+                          color: "#141138",
+                        }}
+                        activeClassName="active"
+                        to="/contact"
+                      >
+                        {" "}
+                        Contact Us
+                      </NavLink>
+                    </li>
+                    
+
+                  </ul>
+                </div>
+              )}
+            </div>
+          </div>
+    </div>
+  
+)};
 
