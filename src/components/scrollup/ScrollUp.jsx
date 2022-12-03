01028755249d1e7bcf9a5cd521ba151df4676c65
boaz serem <boaserem022@gmail.com>
2022-12-03 15:26:22 +0300

    added the scroll up button

---
+++ src/components/scrollup/ScrollUp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { VerticleButton as ScrollUpButton } from "react-scroll-up-button"; 
+
+
+export const ScrollUp = () => {
+  return (
+    <div>
+      <ScrollUpButton
+        style={{ backgroundColor: "#1055db", border: "4px solid #1055db", borderRadius: "8px", right:"1rem" }}
+      />
+    </div>
+  );
+}
+
+
