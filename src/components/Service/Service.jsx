1482e335d89166abf2773cc21cac259f91f3327c
boaz serem <boaserem022@gmail.com>
2022-12-04 11:40:26 +0300

    animated services component

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import "./Service-style/Service-style.css";
 import Image from "./assets/web-development.svg";
 // import Product from "../Product/Product";
+import Zoom from 'react-reveal/Zoom';
+import Fade from 'react-reveal/Fade';
 // import Stack from "../Stack/Stack";
 import { stackIcons } from "./assets/stacks";
 import Button from "../Button/Button";
@@ -33,6 +35,7 @@ const Service = () => {
           </div>
           <div className="web-services">
             <div className="right">
+            <Zoom bottom cascade>
               {sites.map(({ icon, text }, i) => {
                 return (
                   <p>
@@ -46,11 +49,14 @@ const Service = () => {
                   </p>
                 );
               })}
+              </Zoom>
             </div>
             <div className="left">
               <div className="right">
+              <Zoom bottom cascade>
                 {sites.map(({ icon, text }, i) => {
                   return (
+                    
                     <p>
                       <img
                         src={icon}
@@ -62,6 +68,7 @@ const Service = () => {
                     </p>
                   );
                 })}
+                </Zoom>
               </div>
             </div>
           </div>
@@ -70,7 +77,9 @@ const Service = () => {
             <h2 style={{ color: "rgba(0, 0, 0, 0.7)" }}>Tech Stack</h2>
             {stackIcons.map((image) => {
               return (
+                <Fade right cascade>
                 <img src={image} style={{ marginRight: "1rem" }} alt="sdsdd" />
+                </Fade>
               );
             })}
           </div>
